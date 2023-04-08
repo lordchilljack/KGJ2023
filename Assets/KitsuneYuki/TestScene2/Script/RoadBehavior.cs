b0604c343356f1cff8641a9657c5e47b70edc6ca
InariYuki <105775231+InariYuki@users.noreply.github.com>
2023-04-08 21:18:54 +0800

    道路前進後退++

---
+++ Assets/KitsuneYuki/TestScene2/Script/RoadBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadBehavior : MonoBehaviour
+{
+    public bool reverse = false;
+    [SerializeField] private List<Transform> roadBlocks = new List<Transform>();
+    [SerializeField] private Transform spawnPoint , endPoint;
+    private float roadSpeed = 0.2f;
+    private void FixedUpdate() {
+        for(int i = 0; i < roadBlocks.Count; i++){
+            if(reverse){
+                roadBlocks[i].position += Vector3.forward * roadSpeed;
+            }
+            else{
+                roadBlocks[i].position -= Vector3.forward * roadSpeed;
+            }
+            if(roadBlocks[i].position.z >= spawnPoint.position.z){
+                roadBlocks[i].position = endPoint.position;
+            }
+            else if(roadBlocks[i].position.z <= endPoint.position.z){
+                roadBlocks[i].position = spawnPoint.position;
+            }
+        }
+    }
+}
